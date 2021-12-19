9525e2f35bc1c69a02a38a26e8e531dbb6f97567
Ridwan <ridwan.abdurrasyid@yahoo.com>
2021-12-19 20:56:50 +0700

    UIImageView

--- MIKA/Extension/UIImageView.swift
+++ MIKA/Extension/UIImageView.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 extension UIImageView {
+    
     func loadFromURL(url: String?){
         DispatchQueue.global().async { [weak self] in
             if let url = URL(string: url ?? ""), let data = try? Data(contentsOf: url) {
